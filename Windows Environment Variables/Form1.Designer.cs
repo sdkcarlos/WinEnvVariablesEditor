2e33e822e1fbc54943ab27395ff29a1b7f2fb8a7
Carlos Delgado <sdkcarlos@outlook.com>
2015-08-07 12:39:33 +0200

    Windows Environment Variables Quick Editor
    First commit !

---
+++ Windows Environment Variables/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Windows_Environment_Variables
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.newValue = new System.Windows.Forms.TextBox();
+            this.newName = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.editValue = new System.Windows.Forms.TextBox();
+            this.editName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.editSubmit = new System.Windows.Forms.Button();
+            this.newSubmit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.AllowColumnReorder = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(848, 613);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "#";
+            this.columnHeader1.Width = 29;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Name";
+            this.columnHeader2.Width = 246;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Value";
+            this.columnHeader3.Width = 559;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.newValue);
+            this.groupBox1.Controls.Add(this.newName);
+            this.groupBox1.Location = new System.Drawing.Point(9, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(244, 195);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Create new environment variable";
+            // 
+            // newValue
+            // 
+            this.newValue.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newValue.Location = new System.Drawing.Point(6, 84);
+            this.newValue.Name = "newValue";
+            this.newValue.Size = new System.Drawing.Size(232, 26);
+            this.newValue.TabIndex = 3;
+            // 
+            // newName
+            // 
+            this.newName.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newName.Location = new System.Drawing.Point(6, 38);
+            this.newName.Name = "newName";
+            this.newName.Size = new System.Drawing.Size(232, 26);
+            this.newName.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.editSubmit);
+            this.groupBox2.Controls.Add(this.editValue);
+            this.groupBox2.Controls.Add(this.editName);
+            this.groupBox2.Location = new System.Drawing.Point(9, 225);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(244, 196);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Edit";
+            // 
+            // editValue
+            // 
+            this.editValue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.editValue.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editValue.Location = new System.Drawing.Point(6, 81);
+            this.editValue.Name = "editValue";
+            this.editValue.Size = new System.Drawing.Size(232, 26);
+            this.editValue.TabIndex = 1;
+            // 
+            // editName
+            // 
+            this.editName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.editName.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editName.Location = new System.Drawing.Point(6, 38);
+            this.editName.Name = "editName";
+            this.editName.Size = new System.Drawing.Size(232, 26);
+            this.editName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(1021, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "SDkCarlos 2015";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.splitContainer1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1117, 643);
+            this.panel1.TabIndex = 5;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.newSubmit);
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(1117, 613);
+            this.splitContainer1.SplitterDistance = 848;
+            this.splitContainer1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Purple;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 613);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1117, 30);
+            this.panel2.TabIndex = 5;
+            // 
+            // editSubmit
+            // 
+            this.editSubmit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.editSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.editSubmit.Location = new System.Drawing.Point(6, 124);
+            this.editSubmit.Name = "editSubmit";
+            this.editSubmit.Size = new System.Drawing.Size(232, 56);
+            this.editSubmit.TabIndex = 4;
+            this.editSubmit.Text = "Update";
+            this.editSubmit.UseVisualStyleBackColor = true;
+            this.editSubmit.Click += new System.EventHandler(this.editSubmit_Click);
+            // 
+            // newSubmit
+            // 
+            this.newSubmit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.newSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.newSubmit.Location = new System.Drawing.Point(15, 137);
+            this.newSubmit.Name = "newSubmit";
+            this.newSubmit.Size = new System.Drawing.Size(232, 56);
+            this.newSubmit.TabIndex = 5;
+            this.newSubmit.Text = "Create";
+            this.newSubmit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 643);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Windows Environment Variables Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox newValue;
+        private System.Windows.Forms.TextBox newName;
+        private System.Windows.Forms.TextBox editValue;
+        private System.Windows.Forms.TextBox editName;
+        private System.Windows.Forms.Button editSubmit;
+        private System.Windows.Forms.Button newSubmit;
+    }
+}
+
---
+++ Windows Environment Variables/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_Environment_Variables
+{
+    public partial class Form1 : Form
+    {
+        // Import the kernel32 dll.
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        // The declaration is similar to the SDK function
+        public static extern bool SetEnvironmentVariable(string lpName, string lpValue);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListAllEnvironmentVariables();
+
+            setAppColorFromWindows();
+
+            
+        }
+
+        private void ListAllEnvironmentVariables() 
+        {
+            int count = 1;
+
+            foreach (DictionaryEntry e in System.Environment.GetEnvironmentVariables())
+            {
+                string[] row1 = { e.Key.ToString(), e.Value.ToString() };
+                listView1.Items.Add(count.ToString()).SubItems.AddRange(row1);
+                count++;
+            }
+
+            var compName = System.Environment.GetEnvironmentVariables()["COMPUTERNAME"];
+        }
+
+        /// <summary>
+        /// method for setting the variable of an environment variable associated with
+        /// the current running process
+        /// </summary>
+        /// <param name="variable">variable to set</param>
+        /// <param name="value">value to set the variable to</param>
+        /// <returns></returns>
+        public static bool SetVariable(string variable, string value)
+        {
+            try
+            {
+                // Get the write permission to set the environment variable.
+                EnvironmentPermission permissions = new EnvironmentPermission(EnvironmentPermissionAccess.Write, variable);
+
+                permissions.Demand();
+
+                return SetEnvironmentVariable(variable, value);
+            }
+            catch (SecurityException ex)
+            {
+                MessageBox.Show(string.Format("Error while setting variable {0} : {1}", variable, ex.Message));
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// method for getting all available environment variables
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<string, string> GetEnvironmentVariables()
+        {
+            try
+            {
+                //dictionary object to hold the key/value pairs
+                Dictionary<string, string> variables = new Dictionary<string, string>();
+
+                //loop through the list and add to our dictionary list
+                Parallel.ForEach<DictionaryEntry>(Environment.GetEnvironmentVariables().OfType<DictionaryEntry>(), entry =>
+                {
+                    variables.Add(entry.Key.ToString(), entry.Value.ToString());
+                });
+
+                return variables;
+            }
+            catch (SecurityException ex)
+            {
+                Console.WriteLine("Error retrieving environment variables: {0}", ex.Message);
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// method for retrieving an environment variable by it's name
+        /// </summary>
+        /// <param name="name">the name of the environment variable we're looking for</param>
+        /// <returns></returns>
+        public string GetEnvironmentVariableByName(string name)
+        {
+            try
+            {
+                //get the variable
+                string variable = Environment.GetEnvironmentVariable(name);
+
+                //check for a value, if nothing is returned let the application know
+                if (!string.IsNullOrEmpty(variable))
+                    return variable;
+                else
+                    return "The requested environment variable could not be found";
+            }
+            catch (SecurityException ex)
+            {
+                Console.WriteLine(string.Format("Error searching for environment variable: {0}", ex.Message));
+                return string.Empty;
+            }
+        }
+
+        private bool DoesEnvironmentVariableExist(string variable, EnvironmentVariableTarget target = EnvironmentVariableTarget.User)
+        {
+            try
+            {
+                return string.IsNullOrEmpty(Environment.GetEnvironmentVariable(variable, target)) ? false : true;
+            }
+            catch (SecurityException)
+            {
+                return false;
+            }
+        }
+
+
+
+        /// <summary>
+        /// method for handling environment variable values. Will:
+        /// Update
+        /// Create
+        /// Delete
+        /// </summary>
+        /// <param name="variable">the variable we're looking for</param>
+        /// <param name="value">the value to set it to (null for deleting a variable)</param>
+        /// <param name="target">the targer, defaults to User</param>
+        /// <returns></returns>
+        public void SetEnvironmentVariableValue(string variable, string value = null, EnvironmentVariableTarget target = EnvironmentVariableTarget.User)
+        {
+            try
+            {
+                //first make sure a name is provided
+                if (string.IsNullOrEmpty(variable))
+                    throw new ArgumentException("Variable names cannot be empty or null", "variable");
+
+                if (!DoesEnvironmentVariableExist(variable, target))
+                    throw new Exception(string.Format("The environment variable {0} was not found", variable));
+                else
+                    Environment.SetEnvironmentVariable(variable, value, target);
+
+                Console.WriteLine(string.Format("The environment variable {0} has been deleted", variable));
+
+            }
+            catch (SecurityException ex)
+            {
+                Console.WriteLine(string.Format("Error setting environment variable {0}: {1}", variable, ex.Message));
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(string.Format("Error setting environment variable {0}: {1}", variable, ex.Message));
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                string id = listView1.SelectedItems[0].SubItems[0].Text;
+                string name = listView1.SelectedItems[0].SubItems[1].Text;
+                string value = listView1.SelectedItems[0].SubItems[2].Text;
+
+                editName.Text = name;
+                editValue.Text = value;
+            }
+        }
+
+        public void setAppColorFromWindows()
+        {
+            var color = System.Drawing.Color.Purple;
+
+            try
+            {
+                int argbColor = (int)Microsoft.Win32.Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\DWM", "ColorizationColor", null);
+                color = System.Drawing.Color.FromArgb(argbColor);
+            }
+            catch (ArgumentException){}
+
+
+            newSubmit.FlatAppearance.MouseDownBackColor = color;
+            newSubmit.FlatAppearance.BorderColor = color;
+            editSubmit.FlatAppearance.MouseDownBackColor = color;
+            editSubmit.FlatAppearance.BorderColor = color;
+            panel2.BackColor = color;
+        }
+
+        private void editSubmit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Do you really want to overwrite this environment variable? You can't do undo this change.", "Overwrite environment variable",
+            MessageBoxButtons.YesNo, MessageBoxIcon.Question,
+            MessageBoxDefaultButton.Button1) == System.Windows.Forms.DialogResult.Yes)
+            {
+                //TODO: Stuff
+            }
+        }
+ 
+    }
+}
